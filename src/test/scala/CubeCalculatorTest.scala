7c9153c983b9703ec803137e1255f109c719c58c
Sagar88 <sagar.kulkarni2007@gmail.com>
2020-07-05 19:07:42 +0530

    Update CubeCalculatorTest.scala

--- src/test/scala/CubeCalculatorTest.scala
+++ src/test/scala/CubeCalculatorTest.scala
@@ -2,7 +2,7 @@ import org.scalatest.FunSuite
 
 class CubeCalculatorTest extends FunSuite {
   test("CubeCalculator.cube") {
-    assert(CubeCalculator.cube(2) === 8)
+    assert(CubeCalculator.cube(2) === 9)
     println(" *** Hi **")
   }
 }
